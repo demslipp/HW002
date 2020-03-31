0f9dee49fa4442528887d9ec7c40af634a8d3da6
Test1 <jk1998@yandex.ru>
2020-03-31 15:36:07 +0300

    Initial commit

--- src/Values.java
+++ src/Values.java
@@ -4,39 +4,37 @@ public class Values{
     public Values (){
     }
 
-    double array[]=new double[2];
+    double[] array=new double[2];
     public double maximum;
     public double minimum;
     boolean flag=false;
     Scanner scanner=new Scanner (System.in);
+    int j=0;
 
     public String DoubleValues (){
 
-         for (int i=0; i<array.length; i++){
-              if (scanner.nextDouble ()>0){
+        for (int i=0; i<array.length; i++){
+            if (scanner.nextDouble ()>0){
 
-                   if (! flag){
-                        minimum=array[i];
-                        maximum=array[i];
-                        flag=true;
-                   }
-                   array[i]=scanner.nextDouble();
-              }
-         }
-         for (int i=0; i<array.length; i++){
-              if (array[i]>maximum){
-                   maximum=array[i];
-              }
-              if (array[i]<minimum){
-                   minimum=array[i];
-              }
-         }
-         for (int i=0; i<array.length; i++){
-              System.out.println (array[i]);
-         }
+                if (! flag){
+                    minimum=array[i];
+                    maximum=array[i];
+                    flag=true;
+                }
+                array[j]=scanner.nextDouble ();
+                j++;
+            }
+            if (array[i]>maximum){
+                maximum=array[i];
+            }
+            if (array[i]<minimum){
+                minimum=array[i];
+            }
 
-         String result="Min: "+minimum+" Max: "+maximum;
-         return result;
+            System.out.println (array[i]);
+        }
+
+        return "Min: "+minimum+" Max: "+maximum;
 
     }
 }
